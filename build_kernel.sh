64e249b5dc860a1385e566517b0c61f5026f618a
grace5921 <androidlover5842@gmail.com>
2017-04-18 19:01:36 +0530

    update build_kernel script

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 export ARCH=arm
-export CROSS_COMPILE=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-
-mkdir output
+#export CROSS_COMPILE=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-
 
-make -C $(pwd) O=output msm8916_sec_defconfig VARIANT_DEFCONFIG=msm8916_sec_j5lte_eur_defconfig SELINUX_DEFCONFIG=selinux_defconfig
-make -j64 -C $(pwd) O=output
+make  msm8916_sec_defconfig VARIANT_DEFCONFIG=msm8916_sec_j5lte_eur_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+make -j8
 
-cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
+#cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
